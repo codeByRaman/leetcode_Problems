37cb3ae0d8cd8907771a1500414d1f91d4cc311d
Raman Kumar <ramankumar77520@gmail.com>
2026-07-12 22:19:48 +0530

    Time: 4 ms (38.38%), Space: 43.2 MB (58.02%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,39 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        int n = s.length();
+        for (int i = 0; i < n; i++){
+            char ch = s.charAt(i);
+            if (ch == '(' || ch == '{' || ch == '['){
+                stack.push(ch);
+            }
+            else if (ch == ')' && stack.isEmpty()){
+                return false;
+            }
+            else if (ch == '}' && stack.isEmpty()){
+                return false;
+            }
+            else if (ch == ']' && stack.isEmpty()){
+                return false;
+            }         
+            else if (ch == ')' && stack.peek() == '('){
+                stack.pop();
+            }
+            else if (ch == '}' && stack.peek() == '{'){
+                stack.pop();
+            }
+            else if (ch == ']' && stack.peek() == '['){
+                stack.pop();
+            }
+            else {
+                return false;
+            }
+        }
+        if (stack.isEmpty()){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
