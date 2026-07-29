29b2441f0e8cc19cee0dd48881f5bf86fab7492c
Raman Kumar <ramankumar77520@gmail.com>
2026-07-29 22:55:20 +0530

    Time: 0 ms (100%), Space: 43.9 MB (46.89%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,55 +1,30 @@
 class Solution {
-    public int pivotIndex(int[] nums){
+    public int search(int[] nums, int target) {
         int n = nums.length;
         int left = 0;
         int right = n - 1;
 
-        int index = -1;
         while (left <= right){
             int mid = left + (right - left) / 2;
-            if (nums[mid] <= nums[n - 1]){
-                right = mid - 1;
+
+            if (nums[mid] == target){
+                return mid;
             }
-            else {
-                index = mid;
-                left = mid + 1;
-            }
-        }
-         return index;
-    } 
-    public int search(int[] nums, int target) {
-        int peekIndex = pivotIndex(nums);
-        int n = nums.length;
-
-        if (target <= nums[n-1]){
-            int left = peekIndex + 1;
-            int right = n - 1;
-
-            while (left <= right){
-                int mid = left + (right - left) / 2;
-
-                if(nums[mid] == target){
-                    return mid;
-                }else if(nums[mid] < target){
-                    left = mid + 1;
-                }else{
+            else if (nums[mid] > nums[right]){
+                if (target >= nums[left] && target < nums[mid]){
                     right = mid - 1;
+                }else{
+                    left = mid + 1;
                 }
             }
-        }else {
-            int left = 0;
-            int right = peekIndex;
-
-            while (left <= right){
-                int mid = left + (right - left) / 2;
-
-                if(nums[mid] == target){
-                    return mid;
-                }else if(nums[mid] < target){
+            else if (nums[mid] < nums[right]){
+                if (target > nums[mid] && target <= nums[right]){
                     left = mid + 1;
-                }else{
-                    right = mid - 1;
+                }else {
+                    right = mid -1;
                 }
+            }else{
+                right --;
             }
         }
         return -1;
