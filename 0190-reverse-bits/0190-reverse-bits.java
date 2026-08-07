440f8dca67afa2ab2cb5a6d78fa323cbec8260b4
Raman Kumar <ramankumar77520@gmail.com>
2026-08-07 23:07:00 +0530

    Time: 1 ms (61.46%), Space: 42.4 MB (70.18%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int reverseBits(int n) {
+        int result = 0;
+
+        for (int i = 0; i < 32; i++) {
+            result <<= 1;          // result ko left shift karo
+            result |= (n & 1);     // n ka last bit add karo
+            n >>>= 1;              // n ko unsigned right shift karo
+        }
+
+        return result; 
+    }
+}
