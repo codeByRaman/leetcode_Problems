662880e957ed37bd886aaa1a3ceaec4997fee06f
Raman Kumar <ramankumar77520@gmail.com>
2026-08-29 19:20:37 +0530

    Time: 1 ms (36.11%), Space: 43.3 MB (50.45%) - LeetHub

--- 3884-first-matching-character-from-both-ends/3884-first-matching-character-from-both-ends.java
+++ 3884-first-matching-character-from-both-ends/3884-first-matching-character-from-both-ends.java
@@ -1,16 +1,10 @@
 class Solution {
     public int firstMatchingIndex(String s) {
-        char[] ch = s.toCharArray();
-
-        int n = ch.length;
-
         int left = 0;
-        int right = n - 1;
+        int right = s.length() - 1;
 
         while (left <= right){
-            if (ch[left] == ch[right]){
-                return left;
-            }
+            if (s.charAt(left) == s.charAt(right)) return left;
             left ++;
             right --;
         }
